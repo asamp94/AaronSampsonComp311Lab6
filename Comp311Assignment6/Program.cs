5d8405b666433f9177120490d46864aa162925e2
Aaron Sampson <asampso6@my.centennialcollege.ca>
2025-04-04 00:30:08 -0230

    Initial commit

---
+++ Comp311Assignment6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Comp311Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
